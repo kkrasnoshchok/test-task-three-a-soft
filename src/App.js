e67d73b3fa25e2de0bfffb8297b17f2274913a95
vaedym <krasnoshchokvadim@gmail.com>
2021-12-05 01:30:00 +0200

    already involved local storage, but not fully functionative

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  // создаём ref, чтобы заменить инпут красивой кнопкой
+  const inputFileRef = useRef(null);
+
+  useEffect(() => {
+    console.log(localStorage.getItem("state"));
+  });
+
+  let handleInputFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      // setState([...JSON.parse(localStorage.getItem("state")), { fileName: file.name, fileImage: reader.result }]);
+      localStorage.setItem(
+        "state",
+        JSON.stringify([
+          ...JSON.parse(localStorage.getItem("state")),
+          { fileName: file.name, fileImage: reader.result },
+        ])
+      );
+    };
+  };
+
+  // метод map для отображения картинок с именами
+
+  console.log(JSON.parse(localStorage.getItem("state")));
+
+  let imagesGallery = JSON.parse(localStorage.getItem("state")).map((e) => {
+    return (
+      <div className="galleryItem">
+        <img key="lol" src={e.fileImage} alt="" className="img" />
+        <p key="keke">{e.fileName}</p>
+      </div>
+    );
+  });
+
+  console.log(JSON.parse(localStorage.getItem("state")));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <form>
+        <button
+          className="addImageButton"
+          onClick={(event) => {
+            event.preventDefault();
+            inputFileRef.current.click();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          add images
+        </button>
+        <input
+          type="file"
+          accept="image/*"
+          ref={inputFileRef}
+          onChange={handleInputFileChange}
+          style={{ display: "none" }}
+        />
+      </form>
+      <br />
+      <div className="gallery">{imagesGallery}</div>
     </div>
   );
-}
+};
 
 export default App;
